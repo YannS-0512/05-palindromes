e40e64996961380b643dbffe3cf299538b86af50
YannS-0512 <yann.sfar@edu.esiee.fr>
2024-11-14 10:19:55 +0000

    exo palindromes

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 #### Fonction secondaire
+"""palindrome code"""
 
 
 def ispalindrome(p):
+    """return boolean if string is palindrome or not"""
+    p = p.lower()
+    table = str.maketrans("àâäéèêëîïôöùûüç", "aaaeeeeiioouuuc",  " !$%&'()*+,-./:;<=>?@[]^_`{|}~ ")
 
-    # votre code ici
-    
-    return False
+    result = p.translate(table)
+    return bool(result == result[::-1])
 
 #### Fonction principale
 
 
 def main():
-
+    """methode main"""
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
